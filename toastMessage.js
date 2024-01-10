f562154fd98215cc16cfdca7711f320163bb6d73
張永昌 <josh22788@gmail.com>
2024-01-10 22:47:38 +0800

    Update toastMessage.js
    Update JavaScript.
    - Fix  the issue for creating extra useless divs.

--- toastMessage.js
+++ toastMessage.js
@@ -7,78 +7,35 @@
   message.error({ type: 'error', content: 'error' })
 */
 
-const toastRootId = 'toast-root';
+const toastRootId = 'toast-root'
 
 /**
  * 建立吐司訊息容器
  * @returns 吐司訊息容器
  */
 function createToastRoot() {
-  let toastRoot;
-  let toastContainer;
+  let toastRoot
+  let toastContainer
   if (document.getElementById(toastRootId)) {
-    toastRoot = document.getElementById(toastRootId);
+    toastRoot = document.getElementById(toastRootId)
   } else {
-    const divDOM = document.createElement('div');
-    divDOM.id = toastRootId;
-    document.body.appendChild(divDOM);
-    toastRoot = divDOM;
+    const divDOM = document.createElement('div')
+    divDOM.id = toastRootId
+    document.body.appendChild(divDOM)
+    toastRoot = divDOM
   }
   if (toastRoot.firstElementChild) {
-    toastContainer = toastRoot.firstElementChild;
+    toastContainer = toastRoot.firstElementChild
   } else {
-    const divDOM = document.createElement('div');
-    divDOM.className = 'toast-container';
-    toastRoot.appendChild(divDOM);
-    toastContainer = divDOM;
+    const divDOM = document.createElement('div')
+    divDOM.className = 'toast-container'
+    toastRoot.appendChild(divDOM)
+    toastContainer = divDOM
   }
-  const divDOM = document.createElement('div');
-  divDOM.id = dayjs().format('YYYYMMDDHHmmssSSS');
-  toastContainer.appendChild(divDOM);
-  return divDOM;
-}
-
-/**
- * 暫停吐司訊息滯留時間
- * @param {String} toastId 吐司訊息容器ID
- */
-function pauseToastTimer(toastId) {
-  const toastElement = document.getElementById(toastId);
-  const duration = parseInt(toastElement.duration);
-  const timeout = parseInt(toastElement.timeout);
-  const pass = parseInt(toastElement.pass) + (new Date().getTime() - timeout);
-  const remainingTime = duration - pass;
-  toastElement.pass = pass;
-  toastElement.remainingTime = remainingTime;
-
-  clearTimeout(toastElement.toastTimeout);
-  clearTimeout(toastElement.toastRemove);
-  toastElement
-    .querySelector('.toast')
-    .style.setProperty('--animation-play-state', 'paused');
-}
-
-/**
- * 恢復吐司訊息滯留時間
- * @param {String} toastId 吐司訊息容器ID
- */
-function resumeToastTimer(toastId) {
-  const toastElement = document.getElementById(toastId);
-  const remainingTime = toastElement.remainingTime;
-  toastElement.timeout = new Date().getTime();
-  toastElement
-    .querySelector('.toast')
-    .style.setProperty('--animation-play-state', 'running');
-
-  toastElement.toastTimeout = setTimeout(() => {
-    toastElement.classList.remove('show');
-    toastElement.classList.add('hide');
-  }, remainingTime);
-
-  toastRemove = setTimeout(() => {
-    toastElement.remove();
-  }, remainingTime + 200);
-  toastElement.toastRemove = toastRemove;
+  const divDOM = document.createElement('div')
+  divDOM.id = new Date().getTime()
+  toastContainer.appendChild(divDOM)
+  return divDOM
 }
 
 /**
@@ -87,80 +44,65 @@ function resumeToastTimer(toastId) {
  * @param {Object} props 吐司訊息類型、內容、滯留時間
  */
 function testToast(container, props) {
-  const { content } = props;
-  let { type, duration } = props;
-  let toastId = container.id;
-  if (type === undefined) {
-    type = 'info';
-  }
+  const { type, content } = props
+  let { duration } = props
+  let toastId = container.id
   if (duration === undefined) {
-    duration = 3000;
+    duration = 5000
   }
-  let icon = ``;
+  let icon = ``
   switch (type) {
     case 'success':
-      icon = `<i class="ri-checkbox-circle-fill ri-fw"></i>`;
-      break;
+      icon = `<i class="ri-checkbox-circle-fill ri-fw"></i>`
+      break
     case 'info':
-      icon = `<i class="ri-information-fill ri-fw"></i>`;
-      break;
+      icon = `<i class="ri-information-2-fill ri-fw"></i>`
+      break
     case 'loading':
-      icon = `<i class="ri-loader-4-fill ri-fw ri-spin"></i>`;
-      break;
+      icon = `<i class="ri-loader-4-fill ri-fw"></i>`
+      break
     case 'warning':
-      icon = `<i class="ri-alert-fill ri-fw"></i>`;
-      break;
+      icon = `<i class="ri-alert-fill ri-fw"></i>`
+      break
     case 'error':
-      icon = `<i class="ri-close-circle-fill ri-fw"></i>`;
-      break;
+      icon = `<i class="ri-close-circle-fill ri-fw"></i>`
+      break
     default:
-      icon = `<i class="ri-information-fill ri-fw"></i>`;
+      icon = `<i class="ri-information-2-fill ri-fw"></i>`
       break;
   }
-  let toastHTML = `<div
-    class="toast ${type} show"
-    style="--duration: ${duration}ms"
-    onmouseenter="pauseToastTimer('${toastId}')"
-    onmouseleave="resumeToastTimer('${toastId}')"
-  >
-    ${icon}
-    <div class='content'>${content}</div>
-  </div>`;
+  let toastHTML = `${icon}<p class='content'>${content}</p></div>`;
+  document.getElementById(toastId).classList = `toast ${type} show`;
+  document.getElementById(toastId).style = `--duration: ${duration}ms`;
   document.getElementById(toastId).innerHTML = toastHTML;
-  let toastTimeout = setTimeout(() => {
-    document.getElementById(toastId).classList.remove('show');
-    document.getElementById(toastId).classList.add('hide');
-  }, duration);
-  let toastRemove = setTimeout(() => {
-    document.getElementById(toastId).remove();
-  }, duration + 200);
-  container.duration = duration;
-  container.pass = 0;
-  container.timeout = new Date().getTime();
-  container.toastTimeout = toastTimeout;
-  container.toastRemove = toastRemove;
-  return toastTimeout;
+  setTimeout(() => {
+    document.getElementById(toastId).classList.remove('show')
+    document.getElementById(toastId).classList.add('hide')
+  }, duration)
+  setTimeout(() => {
+    document.getElementById(toastId).remove()
+  }, duration + 200)
 }
 
 const message = {
   success: (props) => {
-    let toastRootElement = createToastRoot();
-    testToast(toastRootElement, props);
+    let toastRootElement = createToastRoot()
+    testToast(toastRootElement, props)
   },
   info: (props) => {
-    let toastRootElement = createToastRoot();
-    testToast(toastRootElement, props);
+    let toastRootElement = createToastRoot()
+    testToast(toastRootElement, props)
   },
   loading: (props) => {
-    let toastRootElement = createToastRoot();
-    testToast(toastRootElement, props);
+    let toastRootElement = createToastRoot()
+    testToast(toastRootElement, props)
   },
   warning: (props) => {
-    let toastRootElement = createToastRoot();
-    testToast(toastRootElement, props);
+    let toastRootElement = createToastRoot()
+    testToast(toastRootElement, props)
   },
   error: (props) => {
-    let toastRootElement = createToastRoot();
-    testToast(toastRootElement, props);
+    let toastRootElement = createToastRoot()
+    testToast(toastRootElement, props)
   },
-};
+}
